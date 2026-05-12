29ae3a3fb7c5c975e751ddf31ffd3c3c6a76d717
Conor Barcoe <conor.barcoe@playtech.com>
2026-05-13 00:14:27 +0100

    Auto-generated commit

---
+++ chunk-K7J6RGUZ.js
@@ -0,0 +1,1 @@
+function t(r){return r<1e3?r.toString():r<1e4?(r/1e3).toFixed(1).replace(/\.0$/,"")+"k":r<1e6?Math.floor(r/1e3)+"k":(r/1e6).toFixed(1).replace(/\.0$/,"")+"M"}function e(r){return r?r.replace("/video/upload/","/video/upload/so_0,w_400,h_500,c_fill,q_auto,f_auto/").replace(/\.[^.]+$/,".jpg"):""}export{t as a,e as b};
